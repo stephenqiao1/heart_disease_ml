d176cb3328728e11e24b7181f1dd22943e779f9b
Stephen Qiao <stephenqiao123@gmail.com>
2023-08-14 12:59:20 -0700

    finished removing outliers using the std method

--- data_prep.py
+++ data_prep.py
@@ -1,6 +1,7 @@
 import pandas
 from numpy import mean
 from numpy import std
+import matplotlib.pyplot as plt
 
 # Load Data
 filename = "processed.cleveland.data"
@@ -14,6 +15,11 @@ data = pandas.read_csv(filename, names=names)
 # print(data.groupby('num').size())
 
 # find outliers for age
+# check if gaussian 
+# data['age'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
+
 age_mean, age_std = mean(data['age']), std(data['age'])
 print("Age mean:", age_mean)
 print("Age std:", age_std)
@@ -25,14 +31,82 @@ age_outliers = [x for x in data['age'] if x < age_lower or x > age_upper]
 print('Identified outliers in age: %d' % len(age_outliers))
 
 # find outliers for cp
-cp_mean, cp_std = mean(data['cp']), std(data['cp'])
-print("cp mean: %f" % cp_mean)
-print("cp std: %f" % cp_std)
-
-cp_cutoff = cp_std * 3
-cp_lower, cp_upper = cp_mean - cp_cutoff, cp_mean + cp_cutoff
-
-cp_outliers = [x for x in data['cp'] if x < cp_lower or x > cp_upper]
-print('Identified outliers in cp: %d' % len(cp_outliers))
+# check if gaussian 
+# data['cp'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
 
 # find outliers for trestbps
+# check if gaussian
+# data['trestbps'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
+
+trestbps_mean, trestbps_std = mean(data['trestbps']), std(data['trestbps'])
+print("trestbps mean: %f" % trestbps_mean)
+print("trestbps std: %f" % trestbps_std)
+
+trestbps_cutoff = trestbps_std * 3
+trestbps_lower, trestbps_upper = trestbps_mean - trestbps_cutoff, trestbps_mean + trestbps_cutoff
+
+trestbps_outliers = [x for x in data['trestbps'] if x < trestbps_lower or x > trestbps_upper]
+print('Identified outliers in trestbps: %d' % len(trestbps_outliers))
+
+# remove outliers for trestbps
+data = data[(data['trestbps'] >= trestbps_lower) & (data['trestbps'] <= trestbps_upper)]
+# print(trestbps_outliers)
+# print(data.shape)
+
+# find outliers for chol
+# check if gaussian
+# data['chol'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
+
+chol_mean, chol_std = mean(data['chol']), std(data['chol'])
+print("chol mean: %f" % chol_mean)
+print("chol std: %f" % chol_std)
+
+chol_cutoff = chol_std * 3
+chol_lower, chol_upper = chol_mean - chol_cutoff, chol_mean + chol_cutoff
+
+chol_outliers = [x for x in data['chol'] if x < chol_lower or x > chol_upper]
+print("Identified outliers in chol: %d" % len(chol_outliers))
+# print(chol_outliers)
+
+# remove outliers for chol
+data = data[(data['chol'] >= chol_lower) & (data['chol'] <= chol_upper)]
+
+# find outliers for thalach
+# check if gaussian
+# data['thalach'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
+
+thalach_mean, thalach_std = mean(data['thalach']), std(data['thalach'])
+print("thalach mean: %f" % thalach_mean)
+print("thalach std: %f" % thalach_std)
+
+thalach_cutoff = thalach_std * 3
+thalach_lower, thalach_upper = thalach_mean - thalach_cutoff, thalach_mean + thalach_cutoff
+
+thalach_outliers = [x for x in data['thalach'] if x < thalach_lower or x > thalach_upper]
+print("Identified outliers in thalach: %d" % len(thalach_outliers))
+# print(thalach_outliers)
+
+# remove outliers for thalach
+data = data[(data['thalach'] >= thalach_lower) & (data['thalach'] <= thalach_upper)]
+
+# find outliers for oldpeak
+# check if gaussian
+# data['oldpeak'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
+
+# find outliers for ca
+# check if gaussian
+# data['ca'].hist(figsize=(10, 8))
+# plt.tight_layout()
+# plt.show()
+
+print(data.shape)
